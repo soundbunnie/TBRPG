8cf7fb373289a09b78e9b980df8e868c3f94f5c0
Aria <ariari.elk@gmail.com>
2023-01-19 20:48:35 -0600

    Make skills list display current skills

--- Assets/Scripts/Menus/MenuScripts/CharacterMenu.cs
+++ Assets/Scripts/Menus/MenuScripts/CharacterMenu.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
+using TMPro;
 
 public class CharacterMenu : MonoBehaviour
 {
     private Story currentStory;
+    [SerializeField] private GameObject SkillsPanel;
+    [SerializeField] private TextMeshProUGUI SkillsList;
 
     public void OnClick_Back()
     {
@@ -14,11 +17,13 @@ public class CharacterMenu : MonoBehaviour
 
     private void Start()
     {
-        var skills = DialogueManager.GetInstance().GetVariableState("Skills");
-        var player_class = DialogueManager.GetInstance().GetVariableState("player_class");
-
-        Debug.Log(skills);
-        Debug.Log(player_class);
+        var skills = ((Ink.Runtime.ListValue)DialogueManager
+            .GetInstance()
+            .GetVariableState("Skills")).value;
+        var player_class = ((Ink.Runtime.StringValue)DialogueManager
+            .GetInstance()
+            .GetVariableState("player_class")).value;
+        SkillsList.text = skills.ToString();
     }
 }
 
