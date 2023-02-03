bee19ff70c33467562ba4cedb43be2daed058b82
Aria <ariari.elk@gmail.com>
2023-02-02 20:21:40 -0600

    Add reference to selected tab

--- Assets/Scripts/Menus/Codex/TabGroup.cs
+++ Assets/Scripts/Menus/Codex/TabGroup.cs
@@ -9,6 +9,7 @@ public class TabGroup : MonoBehaviour
     public Sprite tabIdle;
     public Sprite tabHover;
     public Sprite tabActive;
+    public TabButton selectedTab;
 
     public void Subscribe(TabButton button)
     {
@@ -23,7 +24,10 @@ public class TabGroup : MonoBehaviour
     public void OnTabEnter(TabButton button)
     {
         ResetTabs();
-        button.background.sprite = tabHover;
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.background.sprite = tabHover;
+        }
     }
 
     public void OnTabExit(TabButton button)
@@ -33,6 +37,7 @@ public class TabGroup : MonoBehaviour
 
     public void OnTabSelected(TabButton button)
     {
+        selectedTab = button;
         ResetTabs();
         button.background.sprite = tabActive;
     }
@@ -41,6 +46,7 @@ public class TabGroup : MonoBehaviour
     {
         foreach(TabButton button in tabButtons)
         {
+            if (selectedTab != null && button == selectedTab) { continue; }
             button.background.sprite = tabIdle;
         }
     }
