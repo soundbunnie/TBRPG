dbaac0b1dd1d3853d7d99b3cc18a8724d0ad390e
Aria <ariari.elk@gmail.com>
2023-02-09 16:42:58 -0600

    Make observation page elements only display if there are multiple pages

--- Assets/Scripts/Observations/ObservationsPanel.cs
+++ Assets/Scripts/Observations/ObservationsPanel.cs
@@ -6,7 +6,27 @@ using TMPro;
 public class ObservationsPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI observationsPanelText;
+    [SerializeField] private TextMeshProUGUI pageCountText;
+    [SerializeField] private GameObject nextPageButton;
+    [SerializeField] private GameObject prevPageButton;
     int currentPage = 1;
+
+    public void Update()
+    {
+        int totalPages = observationsPanelText.textInfo.pageCount;
+        if (totalPages > 1)
+        {
+            pageCountText.text = currentPage.ToString() + "/" + totalPages.ToString();
+            nextPageButton.SetActive(true);
+            prevPageButton.SetActive(true);
+        }
+        else
+        {
+            pageCountText.text = "";
+            nextPageButton.SetActive(false);
+            prevPageButton.SetActive(false);
+        }
+    }
     public void OnClick_NextPage()
     {
         int totalPages = observationsPanelText.textInfo.pageCount;
@@ -22,7 +42,7 @@ public class ObservationsPanel : MonoBehaviour
     {
         int totalPages = observationsPanelText.textInfo.pageCount;
 
-        if (currentPage < totalPages) // Check if there are more pages to switch to
+        if (currentPage != 1) // Check if there are more pages to switch to
         {
             currentPage--;
             observationsPanelText.pageToDisplay--;
